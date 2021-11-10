58f947213fdf6fdd0db9ea340053d4904a47a4cf
Henrique <henriquevitorino19@gmail.com>
2021-11-10 11:47:24 -0300

    Adicionado funcionalidade de converter html para string

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input,setInput] = useState("")
+  const [output,setOutput] = useState("")
+
+  const processarHtml = (html) => {
+    if (html === "") {
+        return "";
+    }
+
+    let result = `${html}`
+    // result = result.replace(/\s/g, '');
+    // REMOVE TABS
+    result = result.replace(/\t/g, '');
+    // REMOVE QUEBRA DE LINHA
+    result = result.replace(/(\r\n|\n|\r)/gm, '');
+    result = result.replaceAll('"', '\\"');
+    result = result.replaceAll('/', '\\/');
+    setOutput(`"${result}"`)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 style={{color: '#ffffff'}}>HTML</h3>
+      <div className="field">
+        <textarea value={input} onChange={(e)=>setInput(e.target.value)}></textarea>
+      </div>
+      <div>
+        
+        <button className="btn" onClick={()=>processarHtml(input)}>Processar</button>
+      </div>
+      <div className="field">
+        <textarea value={output} onChange={(e)=>setOutput(e.target.value)}></textarea>
+      </div>
     </div>
   );
 }
